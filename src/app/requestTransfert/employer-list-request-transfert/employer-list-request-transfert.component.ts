08ec00ea4216125cff37c1fa6e5e35301262b1b5
AmiraJaballah <jaballah.amira22@gmail.com>
2022-06-26 04:00:55 +0100

    fix date

--- src/app/requestTransfert/employer-list-request-transfert/employer-list-request-transfert.component.ts
+++ src/app/requestTransfert/employer-list-request-transfert/employer-list-request-transfert.component.ts
@@ -99,4 +99,19 @@ export class EmployerListRequestTransfertComponent implements OnInit {
   onShowEntryVoucher(req: RequestTransfert) {
     this.router.navigateByUrl("/requestTransfert/entryVoucher/" + req.id);
   }
+  formaterDate(date){
+    if(date){
+      let d = new Date(date)
+      let month = '' + (d.getMonth() + 1)
+      let day = '' + d.getDate()
+      let year = d.getFullYear();
+   
+       if (month.length < 2) 
+           month = '0' + month;
+       if (day.length < 2) 
+           day = '0' + day;
+   
+       return [year, month, day].join('-');
+    }
+  }
 }
